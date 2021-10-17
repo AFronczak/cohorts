321c9f4c9ffa4b1269a7dc5777ce8bc270d2edfc
Adam Fronczak <afronczak@Anastasias-MacBook-Pro.local>
2021-10-17 19:38:54 -0400

    Adds migration, models, root path variable, and seeds.

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require './models'
 
 set :database_file, "config/database.yml"
+set :root_path, File.dirname(__FILE__)
 
 get '/' do
-  # if app becomes modular, will need to use this
-  # register Sinatra::ActiveRecordExtension 
+  @users = Users.order('created_at ASC')
+  @cohorts
   erb :cohorts
 end 
