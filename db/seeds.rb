32402cef6cdd3a9de8904c2e21ff124e66c5687d
Adam Fronczak <afronczak@Anastasias-MacBook-Pro.local>
2021-10-17 19:43:52 -0400

    Remove commented code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,3 @@ end
 
 seed_from_csv(User, '/lib/seeds/users.csv')
 seed_from_csv(Order, '/lib/seeds/orders.csv')
-
-
-# orders_csv_text = File.read(File.join(Sinatra::Application.root_path, '/lib/seeds/orders.csv'))
-# orders_csv = CSV.parse(orders_csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# orders_csv.each do |row|
-#   Order.create(row.to_hash)
-# end
-
-# users_csv_text = File.read(File.join(Sinatra::Application.root_path, '/lib/seeds/users.csv'))
-# users_csv = CSV.parse(users_csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# users_csv.each do |row|
-#   User.create(row.to_hash)
-# end
