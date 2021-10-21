5b24fc0acbfa8d0a7f9e204290e34f5ce288e9d5
Adam Fronczak <fronczak.adam@gmail.com>
2021-10-21 00:41:03 -0400

    Working with selectable cohorts.

--- app.rb
+++ app.rb
@@ -10,46 +10,15 @@ set :database_file, "config/database.yml"
 set :root_path, File.dirname(__FILE__)
 
 get '/' do
-  last_date = User.last.created_at.to_datetime
-  @cohorts = loop_through_dates_return_users(last_date)
+  @cohort_count = 8
+  @cohorts = Order.cohorts(@cohort_count)
 
   erb :cohorts
 end 
 
-def loop_through_dates_return_users(date)
-  earliest_date = Order.select("MIN(created_at)")[0].min
-  last_date = User.last.created_at
+post '/' do
+  @cohort_count = params[:cohort_count].to_i
+  @cohorts = Order.cohorts(@cohort_count)
 
-  data = Order.joins("JOIN (SELECT users.id, MIN(users.created_at) as cohort_date FROM users  WHERE (created_at > '#{earliest_date}') GROUP BY id) AS cohorts ON orders.user_id = cohorts.id").
-  select("orders.user_id, orders.id, orders.order_num, orders.created_at, cohort_date, FLOOR(extract(epoch from (orders.created_at - cohort_date))/604800) as periods_out").where("orders.created_at > '#{earliest_date}' and orders.created_at < '#{last_date}'")
-
-  users = User.where("created_at > '#{earliest_date}' and created_at < '#{last_date}'")
-
-  unique_data = data.all.uniq{|d| [d.send("id"), d.cohort_date, d.periods_out] }
-
-  analysis = unique_data.group_by{|d| Time.parse(d.cohort_date.to_s + " UTC").at_beginning_of_week.to_date}
-  cohort_hash =  Hash[analysis.sort_by { |cohort, data| cohort }.reverse]
-
-  user_buckets = users.all.group_by{|d| Time.parse(d.created_at.to_s + " UTC").at_beginning_of_week.to_date}
-  table = {}
-
-  cohort_hash.each do |arr|
-    periods = []
-    first_time = []
-  
-    table[arr[0]] = {}
-
-    # method for orderers
-    cohort_hash.size.times{|i| periods << arr[1].count{|d| d.periods_out.to_i == i} if arr[1]}
-
-    # method for first time orderers        
-    cohort_hash.size.times{|i| first_time << arr[1].count{|d| if i.eql?(0); d.periods_out.to_i == i && d.order_num.to_i >= 1; else; d.periods_out.to_i == i && d.order_num.to_i == 1; end} if arr[1]}
-    table[arr[0]][:dates] = "#{arr[0].at_beginning_of_week.try(:strftime, "%m/%d")}-#{arr[0].at_end_of_week.try(:strftime, "%m/%d")}"
-    table[arr[0]][:count] = periods
-    table[arr[0]][:total_users] = user_buckets[arr[0]].length
-    table[arr[0]][:first_time] = first_time
-    table[arr[0]][:data] = arr[1]
-  end
-
-  return table
+  erb :cohorts
 end
--- models.rb
+++ models.rb
@@ -4,4 +4,43 @@ end
 
 class Order < ActiveRecord::Base
 	belongs_to :user
+
+	def self.cohorts(count)
+		earliest_date = Order.first.created_at
+		last_date = User.last.created_at
+	
+		data = Order.joins("JOIN (SELECT users.id, MIN(users.created_at) as cohort_date FROM users  WHERE (created_at > '#{earliest_date}') GROUP BY id) AS cohorts ON orders.user_id = cohorts.id").
+		select("orders.user_id, orders.id, orders.order_num, orders.created_at, cohort_date, FLOOR(extract(epoch from (orders.created_at - cohort_date))/604800) as periods_out").where("orders.created_at > '#{earliest_date}' and orders.created_at < '#{last_date}'")
+	
+		users = User.where("created_at > '#{earliest_date}' and created_at < '#{last_date}'")
+	
+		unique_data = data.all.uniq{|d| [d.send("id"), d.cohort_date, d.periods_out] }
+	
+		analysis = unique_data.group_by{|d| d.cohort_date.at_beginning_of_week.to_date}
+
+		cohort_hash = Hash[analysis.sort_by { |cohort, data| cohort }.reverse]
+
+		user_buckets = users.all.group_by{|d| d.created_at.at_beginning_of_week.to_date}
+		table = {}
+
+		cohort_hash.keys[0...count].each do |key|
+			arr = cohort_hash[key]
+			periods = []
+			first_time = []
+		
+			table[key] = {}
+	
+			# orderers
+			cohort_hash.size.times{|i| periods << arr.count{|d| d.periods_out.to_i == i} if arr}
+	
+			# first time orderers     
+			cohort_hash.size.times{|i| first_time << arr.count{|d| if i.eql?(0); d.periods_out.to_i == i && d.order_num.to_i >= 1; else; d.periods_out.to_i == i && d.order_num.to_i == 1; end} if arr}
+			table[key][:date] = key
+			table[key][:count] = periods
+			table[key][:total_users] = user_buckets[key].length
+			table[key][:first_time] = first_time
+			table[key][:data] = arr
+		end
+		return table
+	end
 end
